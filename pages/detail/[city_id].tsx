0eba96871a38a34faecc5974e56d448449fe82ef
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-04-05 16:37:02 -0500

    Styled the page

--- pages/detail/[city_id].tsx
+++ pages/detail/[city_id].tsx
@@ -9,7 +9,7 @@ import WeatherData from "@/interfaces/weather";
 let Cities = cities as CityData[]
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const {city_id} = context.query
+  const { city_id } = context.query
 
   const city = Cities.find((city) => city.id.toString() == city_id)
 
@@ -40,29 +40,41 @@ type Props = {
   weather: WeatherData
 }
 
-export default function({city, weather}: Props) {
+export default function ({ city, weather }: Props) {
   const iconUrl = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
   return (
     <>
       <Head>
         <title>WeatherWise</title>
       </Head>
-      <main>
-        <div className="container">
-          <Link href="/">
-            &larr; Home
-          </Link>
-          <h1>{city.name} ({city.country})</h1>
-          <h2>
-            {weather.main.temp_max.toFixed(0)}&deg;C {weather.main.temp_min.toFixed(0)}&deg;C 
-      
-          </h2>
-          <div>
-            {weather.weather[0].description}
+      <main className="mt-5 mx-5">
+        <h1 className="text-xl font-medium mb-4">WeatherWise</h1>
+        <Link href="/">
+          &larr; Home
+        </Link>
+        <div className="py-5">
+          <div className="bg-blue-500 rounded p-4">
+            <div className="grid grid-cols-2">
+              <div>
+                <h2 className="text-2xl mb-4 text-white">{city.name} ({city.country})</h2>
+                <span className="font-medium text-lg text-white">
+                  {weather.main.temp_max.toFixed(0)}&deg;C
+                </span>
+                &nbsp;
+                <span className="text-gray-300 text-sm">
+                  {weather.main.temp_min.toFixed(0)}&deg;C
+                </span>
+              </div>
+              <div className="justify-self-end">
+                <Image src={iconUrl} width={50} height={50} alt="Weather Icon" />
+                <div className="text-white text-sm">
+                  {weather.weather[0].description}
+                </div>
+              </div>
+            </div>
           </div>
-          <Image src={iconUrl} width={50} height={50} alt="Weather Icon" />
         </div>
       </main>
-    </>  
+    </>
   )
 }
